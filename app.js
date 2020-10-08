9a211565802620590f8143bd6e1e1dbf3a22e09b
SilvenLEAF <SilvenLEAF@gmail.com>
2020-10-08 12:19:03 +0530

    basic MongoDB setup

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ app.use(express.static(path.join(__dirname, `/client/build`)));
 
 
 
+
+
 /* --------------------------------------
 .                   config
 -------------------------------------- */
+require('./config/mongoDBConfig');
+
 
 
 
