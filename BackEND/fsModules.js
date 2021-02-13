81e230ee263d6dfc22ef75b5ea13ef29d4dac452
SilvenLEAF <SilvenLEAF@gmail.com>
2021-02-13 10:25:16 +0530

    fsModule

---
+++ BackEND/fsModules.js
@@ -0,0 +1,18 @@
+// here I'm requiring this file to use those methods
+const fs = require('fs');
+
+
+
+// read file
+const theTextThatComputerRead = fs.readFileSync("readThisFile.html", "utf8");
+// write file
+fs.writeFileSync('writeThisFile.js', theTextThatComputerRead)
+// delete file
+fs.unlinkSync('writeThisFile.js')
+
+
+// create folder
+fs.mkdirSync("myFolder");
+// delete folder
+fs.rmdirSync("myFolder");
+
