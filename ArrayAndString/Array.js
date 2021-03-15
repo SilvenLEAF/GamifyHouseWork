79fe8e0eabf5aedfa2fc366e8748dbce5c271fd7
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-15 08:01:25 +0530

    Array Parent

---
+++ ArrayAndString/Array.js
@@ -0,0 +1,9 @@
+const arr = ['a','b','c','d','e','f','g','h','i','j'];
+const arr2 = [0,1,2,3,4,5,6,7,8,9];
+const arr3 = [
+  { name: 'Naruto', age: 25, isEvil: false },
+  { name: 'Hinata', age: 16, isEvil: false },
+  { name: 'Sasuke', age: 29, isEvil: true },
+  { name: 'Gaara', age: 27, isEvil: false },
+  { name: 'Orochimaru', age: 20, isEvil: true },
+];
