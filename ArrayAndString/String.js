bf9d4c79742d14199e3771b2b9583c90d488855a
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-16 08:11:04 +0530

    String charAt(index)

--- ArrayAndString/String.js
+++ ArrayAndString/String.js
@@ -1,3 +1,10 @@
 const str1 = `Nihao, wo shi SilvenLEAF.`
 const str2 = `Wo shi yi ge quan zhan gongchengshi.`
 const str3 = `Wo feichang xihuan ma daima.`
+
+
+
+// 1. charAt(index) --returns the character of the given index
+console.log(
+  str1.charAt(str1.length - 3) //the last character of a string is [str.length - 1] (because it's ZERO based)
+)
