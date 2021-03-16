edfebe803498ae45b333eb472cc9e4de139c95cd
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-16 08:13:10 +0530

    String charCodeAt(index)

--- ArrayAndString/String.js
+++ ArrayAndString/String.js
@@ -8,3 +8,8 @@ const str3 = `Wo feichang xihuan ma daima.`
 console.log(
   str1.charAt(str1.length - 3) //the last character of a string is [str.length - 1] (because it's ZERO based)
 )
+
+// 2. charCodeAt(index) --returns the Unicode of the character of the given index
+console.log(
+  str1.charCodeAt(str1.length - 3) //the last character of a string is [str.length - 1] (because it's ZERO based)
+)
