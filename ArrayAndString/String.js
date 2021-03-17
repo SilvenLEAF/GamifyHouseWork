4eaa5108739c585929cfb0c3d161376d42eb6c40
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-17 08:25:49 +0530

    String endsWith(string, length)

--- ArrayAndString/String.js
+++ ArrayAndString/String.js
@@ -1,6 +1,6 @@
 const str1 = `Nihao, wo shi SilvenLEAF.`
-const str2 = `Wo shi yi ge quan zhan gongchengshi.`
-const str3 = `Wo feichang xihuan ma daima.`
+const str2 = ` Wo shi yi ge quan zhan gongchengshi.`
+const str3 = ` Wo feichang xihuan ma daima.`
 
 
 
@@ -17,4 +17,9 @@ console.log(
 // 3. concat(arr2, arr3, ...) --returns a new string by concatenating others
 console.log(
   str1.concat(str2, str3) //it concatenates in the order of the given strings
+)
+
+// 4. endsWith(string, length) --returns a boolean whether the parent string (at the given length) ends with that given string, by default this length is the full parent string
+console.log(
+  str1.endsWith(`wo`, 9) //by default it searches full parent string if the length is not given
 )
