d57fee1a2c3dcaed165afefae25bdf8e58034f8a
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-18 08:34:20 +0530

    Simple Upload

--- Upload/app.js
+++ Upload/app.js
@@ -8,16 +8,33 @@ const multer = require('multer');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, `client/build`)));
+app.use(express.static(path.join(__dirname, `client`)));
 
+
+
+/* ------------------------------------
+.        multer settings (upload)
+------------------------------------ */
+var upload = multer({ dest: 'uploads/' })
 
 
 /* ------------------------------------
 .              routes (upload)
 ------------------------------------ */
-app.post('/upload', (req, res, next)=>{
+app.get('/', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, `client/index.html`));
+})
+
+app.post('/upload', upload.single('avatar'), (req, res, next)=>{
   try {
-    res.send('HI!')
+    const reqFile = req.file;
+    const reqBody = req.body;
+
+    console.log(reqFile, reqBody);
+    res.send(`HI`)
+
+
+    
   } catch (err) {
     next(err, req, res);
   }
@@ -35,3 +52,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
   console.log(`Server is running on port ${ PORT }`);
 })
+
+
+
+
+function imageFilter (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+      req.fileValidationError = 'Only image files are allowed!';
+      return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
