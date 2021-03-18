8844aeea4bac2e2403c65d9381b6aacc57f571d3
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-18 07:37:29 +0530

    Errors handler and demo route

--- Upload/app.js
+++ Upload/app.js
@@ -12,6 +12,24 @@ app.use(express.static(path.join(__dirname, `client/build`)));
 
 
 
+/* ------------------------------------
+.              routes (upload)
+------------------------------------ */
+app.post('/upload', (req, res, next)=>{
+  try {
+    res.send('HI!')
+  } catch (err) {
+    next(err, req, res);
+  }
+})
+// errors handler
+app.use((err, req, res, next)=>{
+  console.log(err.message);
+  console.log(err);
+  res.json({ msg: `Server error!`, error: err.message });
+})
+
+
 // --------------------------LISTEN
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
